f325cbff7172433c2d5c0f24bd06e02869b7c0d9
lloy0076 <lloy0076@adam.com.au>
2020-05-06 12:28:35 -0400

    Introduce redux and boom.
    INCREMENT / DECREMENT are implemented.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const App = require('./lib/App');
 const logger = require('./lib/logger');
+const store = require('./lib/Store');
 
-const main = new App(logger);
+const main = new App({
+    logger,
+    store,
+});
 
 main.run().catch((error) => {
     logger.error(error.message, { error });
